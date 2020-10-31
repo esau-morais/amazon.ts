d24c642454c3fbc0d735ad6653788d4650d827f8
esau-morais <esaumorais7@gmail.com>
2020-10-31 19:11:24 -0300

    fix: Update firebase settings

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-aO6xr0fidVeNzqVrACv_yiQ6sANTu6c",
-  authDomain: "ui-clone.firebaseapp.com",
-  databaseURL: "https://ui-clone.firebaseio.com",
-  projectId: "ui-clone",
-  storageBucket: "ui-clone.appspot.com",
-  messagingSenderId: "978388534095",
-  appId: "1:978388534095:web:a5da7c2b28a1189af6fb3a",
-  measurementId: "G-231424SW0V"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
